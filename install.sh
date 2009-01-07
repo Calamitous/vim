db1b6eda631ec7ec93a6ecd42e9b227222f63c03
Eric Budd <ericbudd@calamitylane.com>
2009-01-06 21:55:58 -0600

    Added swp and backup directories for less confusing install

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/bash
+#!/usr/bin/ruby
 
 # Back up existing vim config
-mv ~/.vim ~/.vim_bu
-mv ~/.vimrc ~/.vimrc_bu
+
+(`mv ~/.vim ~/.vim_bu`) if File.exists?('~/.vim')
+(`mv ~/.vimrc ~/.vimrc_bu`) if File.exists?('~/.vimrc')
 
 # Copy over new config
-mv * ~/.vim
-mv ~/.vim/.vimrc ~/.vimrc
-
+`mkdir ~/.vim`
+`cp -r * ~/.vim`
+`mv ~/.vim/.vimrc ~/.vimrc`
 
